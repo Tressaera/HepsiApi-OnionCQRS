6fab986495385d981086a605f14081a5c38342b6
= <mattshady1067@gmail.com>
2024-05-02 11:16:09 +0300

    temel Entity'ler oluşturuldu.

--- Core/HepsiApi.Domain/Entities/Product.cs
+++ Core/HepsiApi.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HepsiApi.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HepsiApi.Domain.Entities
 {
-    public class Product
+    public class Product:EntityBase
     {
         public required string Title { get; set; }
         public required string Description { get; set; }
---
+++ Infrastructure/HepsiApi.Persistence/Configuration/ProductConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using HepsiApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Persistence.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker=new("tr");
+            Product product1 = new()
+            {
+                Id = 1,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 1,
+                Discount = faker.Random.Decimal(6, 18),
+                Price = faker.Finance.Amount(18, 1800),
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            }; 
+            Product product2 = new()
+            {
+                Id = 2,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 3,
+                Discount = faker.Random.Decimal(6, 18),
+                Price = faker.Finance.Amount(18, 1800),
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            };
+            builder.HasData(product1,product2);
+        }
+    }
+}
